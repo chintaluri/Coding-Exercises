5b628b5b55e262e02ac80c22ad43c6e122cdb4ac
Your Name <[you@example.com](mailto:you@example.com)>
2019-02-21 22:22:38 -0500

    python practice file 1

---
+++ p-prac-ex1.py
@@ -0,0 +1,25 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+# Extras:
+#
+# Add on to the previous program by asking the user for another number and printing out that many copies of the previous message.
+# (Hint: order of operations exists in Python)
+# Print out that many copies of the previous message on separate lines.
+# (Hint: the string "\n is the same as pressing the ENTER button)
+
+
+
+name = input('Please give me your name! ')
+age  = int(input('Please give me your age '))
+calcul = str(2019-(100-age))
+another_number = int(input('How many times would you like the above statement to be printed? '))
+
+print(('Hello'+ name +' you will be 100 years old in the year' + calcul + '\n')*another_number)
+
+
+
+
+
+
+                                                                      
